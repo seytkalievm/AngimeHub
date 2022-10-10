6cee80e63de420ed200bdd3031f1a41b8bc030d9
Kamila-Khamidullina <kh205601@gmail.com>
2022-10-10 22:00:25 +0300

    editing the workflow

--- tasks.py
+++ tasks.py
@@ -24,13 +24,13 @@ def spellcheck(c):
         )
         incorrect_words = set(aspell_output.split("\n")) - {""} - known.words
         if len(incorrect_words) > 0:
-            out = f"In {tex_path} the following words are not known: <br />"
+            out = f"### :x: In {tex_path} the following words are not known: <br />"
             for string in sorted(incorrect_words):
                 out += f"{string}<br />"
             exit_code = 1
 
         else:
-            out += f"In {tex_path} everything is fine!<br />"
+            out += f"### :heavy_check_mark: In {tex_path} everything is fine!<br />"
 
         out += '<br />'
     
